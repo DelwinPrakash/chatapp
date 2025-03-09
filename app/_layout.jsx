216b7e4f325c344a667e75903d10e5a578fd02e2
= <delwinprakash499@gmail.com>
2025-03-09 12:49:34 +0530

    layout changes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{headerShown: false}}>
       <Stack.Screen name="index" options={{title: "ChatApp", headerShown: true, headerStyle:{backgroundColor: "#3e787a"}, headerShadowVisible: false}}/>
-      <Stack.Screen name="chat/index" options={{title: "Chat 1", headerShown: true}}/>
+      <Stack.Screen name="chat/index" options={{title: "Chat 1", headerShown: true, headerStyle:{backgroundColor: "#3e787a"}, headerShadowVisible: false}}/>
     </Stack>
   );
 }
