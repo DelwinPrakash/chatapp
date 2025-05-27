49df2fec7dbd755d9e73f9a6c92ff55f3bdc5dae
= <delwinprakash499@gmail.com>
2025-05-27 14:32:47 +0530

    layout fix on home screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
   return (
     <AuthProvider>
       <Stack screenOptions={{headerShown: false}}>
-        <Stack.Screen name="index" options={{title: "ChatApp", headerShown: true, headerStyle:{backgroundColor: "#3e787a"}, headerShadowVisible: false}}/>
+        <Stack.Screen name="index" options={{title: "ChatApp", headerStyle:{backgroundColor: "#3e787a"}, headerShadowVisible: false}}/>
         <Stack.Screen name="chat/index" options={{title: "Chat 1", headerShown: true, headerStyle:{backgroundColor: "#3e787a"}, headerShadowVisible: false}}/>
         <Stack.Screen name="auth/index"/>
       </Stack>
